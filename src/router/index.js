1777b45aa50150b197cf8e389e661376a2827f6b
myst5th <1192445074@qq.com>
2022-05-04 10:47:48 +0800

    更新Login组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Login from '@/views/Login';
+import Login from '@/views/Login/Login';
 
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/', redirect: '/home'
-    },
-    {
-        path: '/login', component: Login
-    }
+    {path: '/', redirect: '/home'},
+    {path: '/login', component: Login},
 ];
 
 const router = new VueRouter({
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/home') {
+        const token = localStorage.getItem('token');
+        if (token) {
+            next();
+        } else {
+            next('/login');
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login-container">
+    <h2>用户登录</h2>
+    <div class="login-box">
+      <input type="text" required v-model="username">
+      <label>用户名</label>
+    </div>
+    <div class="login-box">
+      <input type="password" required v-model="password">
+      <label>密码</label>
+    </div>
+    <a href="javascript:void(0)" @click.prevent="login">
+      登录
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </a>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/login.less';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      console.log('login...');
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  padding: 40px;
+  background-color: rgba(0, 0, 0, 0.2);
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.4);
+}
+</style>
